408f016e34ca3b8ead3e6d346a7701a21e9f1272
Lakshya Jain <lakshyajain.14022@gmail.com>
2024-09-03 23:42:43 +0530

    updated faq section and more

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -13,64 +13,64 @@ import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(MotionPathPlugin);
 
-const Hero = ({ aboutRef, ctfRef, venueRef }) => {
+const Hero = ({ aboutRef, ctfRef, venueRef , faqRef}) => {
 
-    useGSAP(() => {
+    // useGSAP(() => {
 
-        const tl = gsap.timeline();
+    //     const tl = gsap.timeline();
 
-        gsap.to('.animation-opacity', {
-            opacity: 0,
-            duration: 0
-        })
+    //     gsap.to('.animation-opacity', {
+    //         opacity: 0,
+    //         duration: 0
+    //     })
 
-        tl.to("#rocket", {
-            motionPath: {
-                path: "#path",
-                align: "#path",
-                alignOrigin: [0.5, 0.5],
-            },
-            duration: 3,
-            ease: "power1.inOut",
-            rotation: "-=45",
-        });
+    //     tl.to("#rocket", {
+    //         motionPath: {
+    //             path: "#path",
+    //             align: "#path",
+    //             alignOrigin: [0.5, 0.5],
+    //         },
+    //         duration: 3,
+    //         ease: "power1.inOut",
+    //         rotation: "-=45",
+    //     });
 
-        tl.to("#rocket", {
-            y: "-=500",
-            rotation: "-10",
-            opacity: 0,
-            duration: 1,
-            ease: "power1.in",
-            display: 'none'
-        });
+    //     tl.to("#rocket", {
+    //         y: "-=500",
+    //         rotation: "-10",
+    //         opacity: 0,
+    //         duration: 1,
+    //         ease: "power1.in",
+    //         display: 'none'
+    //     });
 
-        setTimeout(() => {
-            tl.to(".animation-opacity", {
-                opacity: 1,
-                duration: 0.25,
-                ease: "power1.in"
-            })
+    //     setTimeout(() => {
+    //         tl.to(".animation-opacity", {
+    //             opacity: 1,
+    //             duration: 0.25,
+    //             ease: "power1.in"
+    //         })
 
 
-            tl.to(".path-rocket-div", {
-                display: 'none',
-                duration: 0
-            })
+    //         tl.to(".path-rocket-div", {
+    //             display: 'none',
+    //             duration: 0
+    //         })
 
-            tl.to('.animation-blink', {
-                opacity: 0, 
-                duration: 0.6, 
-                repeat: -1,    
-                yoyo: true,      
-                ease: 'power1.inOut'  
-            });
-        }, 4600)
-    }, [])
+    //         tl.to('.animation-blink', {
+    //             opacity: 0, 
+    //             duration: 0.6, 
+    //             repeat: -1,    
+    //             yoyo: true,      
+    //             ease: 'power1.inOut'  
+    //         });
+    //     }, 4600)
+    // }, [])
 
     return (
         <div className="bg-cover bg-center bg-no-repeat h-screen w-screen bg-[#282729] relative overflow-hidden"
             style={{ backgroundImage: `url(${background})` }}>
-            <Navbar aboutRef={aboutRef} ctfRef={ctfRef} venueRef={venueRef} />
+            <Navbar aboutRef={aboutRef} ctfRef={ctfRef} venueRef={venueRef} faqRef={faqRef}/>
             <div className='flex mx-16 mt-20' id='hero'>
                 <div className='w-3/5 flex flex-col'>
                     <h3 className='text-[#3798D0] font-Minecraft text-[7rem] font-medium tracking-widest animation-opacity'>LNM HACKS</h3>
@@ -87,7 +87,7 @@ const Hero = ({ aboutRef, ctfRef, venueRef }) => {
                         <TopicButton className={'text-xl font-normal tracking-wider ml-16  animation-opacity'} text={`Careers`} /> */}
                     </div>
                     <div id='rocket'>
-                        <img src={Rocket} />
+                        {/* <img src={Rocket} /> */}
                     </div>
                 </div>
                 <div className='flex flex-col w-2/5 items-center animation-opacity'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,22 @@ export default {
         'IBM-PLEX-MONO': ["IBM Plex Mono", "monospace"],
         'Handjet': ["Handjet", 'sans-serif'],
         'Rajdhani': ["Rajdhani", 'sans-serif'],
-        'Pixeboy' : ['Pixeboy', 'sans-serif']
+        'Pixeboy': ['Pixeboy', 'sans-serif'],
+        'Public-sans' : ["Public Sans", 'sans-serif']
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
