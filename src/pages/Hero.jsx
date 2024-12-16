004bdf4451599274140570a5e93751b8eaa818f1
Lakshya Jain <lakshyajain.14022@gmail.com>
2024-12-16 14:09:38 +0530

    hero section responsive

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -31,33 +31,33 @@ const Hero = ({ aboutRef, ctfRef, venueRef, faqRef , scheduleRef }) => {
     }
 
     return (
-        <div className="bg-cover bg-center bg-no-repeat h-[100vh] w-screen bg-[#282729] relative overflow-hidden 2xl:max-w-[1536px] 2xl:max-h-[874px] select-none"
+        <div className="bg-cover bg-center bg-no-repeat h-[97vh] md:h-[100vh] w-screen bg-[#282729] relative overflow-hidden 2xl:max-w-[1536px] 2xl:max-h-[874px] select-none"
             style={{ backgroundImage: `url(${bg_new})` }}>
             <Suspense fallback={<div>Loading...</div>}>
                 <Navbar aboutRef={aboutRef} ctfRef={ctfRef} venueRef={venueRef} faqRef={faqRef} scheduleRef={scheduleRef}/>
             </Suspense>
-            <div className='flex mx-16 mt-20' id='hero'>
+            <div className='flex mx-8 md:mx-16 mt-20' id='hero'>
                 <div className='w-3/5 flex flex-col'>
-                    <h1 className='text-[#3798D0] font-Minecraft text-[7rem] font-medium tracking-widest animation-opacity'>LNM HACKS</h1>
-                    <div className='flex -mt-12'>
-                        <h2 className='text-[#4DC081] font-Minecraft text-[7rem] font-medium tracking-widest animation-opacity'>X</h2>
-                        <h2 className='text-[#ADFFE1] font-Minecraft text-[7rem] font-medium tracking-widest ml-8 animation-opacity'>CTF</h2>
+                    <h1 className={`text-[#3798D0] font-Minecraft text-[4rem] md:text-[7rem] font-medium md:tracking-widest leading-[1] md:leading-normal`}>LNM HACKS</h1>
+                    <div className='flex md:-mt-12'>
+                        <h2 className={`text-[#4DC081] font-Minecraft text-[3rem] md:text-[7rem] font-medium tracking-widest`}>X</h2>
+                        <h2 className={`text-[#ADFFE1] font-Minecraft text-[3rem] md:text-[7rem] font-medium tracking-widest ml-8`}>CTF</h2>
                     </div>
                     <div>
-                        <h3 className='text-[#EE3A23] font-Minecraft text-[4rem] font-medium tracking-widest animation-opacity animation-blink'>Coming Soon</h3>
+                        <h3 className='text-[#EE3A23] mt-4 md:mt-0  font-Minecraft text-[3rem] md:text-[4rem] font-medium leading-[1] md:leading-normal tracking-widest animation-opacity animation-blink'>Coming Soon</h3>
                     </div>
                     <Suspense fallback={<div>Loading...</div>}>
-                        <TopicButton onClick={openRegistrationForm} text={'Pre-Register Now'} className={'text-lg font-normal tracking-wider ml-8 mt-4 w-56 cursor-pointer z-10'} />
+                        <TopicButton onClick={openRegistrationForm} text={'Pre-Register Now'} className={'text-lg font-normal tracking-wider ml-8 mt-8 md:mt-4 w-56 cursor-pointer z-10'} />
                     </Suspense>
                 </div>
-                <div className='flex flex-col w-2/5 items-center animation-opacity'>
-                    <div className='flex mt-4'>
+                <div className='flex flex-col w-2/5 items-center '>
+                    <div className={`mt-4 hidden md:flex`}>
                         <img src={BracketLeft} className='h-44' alt="Bracket Left" loading="lazy" />
                         <img src={Coin} className='h-44' alt="Coin" loading="lazy" />
                         <img src={BracketRight} className='-ml-[2.1rem] h-44' alt="Bracket Right" loading="lazy" />
                     </div>
                     <div>
-                        <img src={Dinosaur} className='h-56 mt-32 ml-20' alt="Dinosaur" loading="lazy"/>
+                        <img src={Dinosaur} className='h-56 -mt-4 md:mt-32 ml-2 md:ml-20' alt="Dinosaur" loading="lazy"/>
                     </div>
                 </div>
             </div>
